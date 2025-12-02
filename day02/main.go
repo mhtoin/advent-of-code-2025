18e85b84a939c690f459e5bd4dec71b79afb576a
Miika Oinonen <mhtoin@utu.fi>
2025-12-02 17:29:38 +0200

    Solve day 2

---
+++ day02/main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/mhtoin/advent-of-code-2025/common"
+)
+
+func main() {
+	solvePart1()
+	solvePart2()
+}
+
+func solvePart1() {
+	file, err := common.GetInputFile(2)
+
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	totalSum := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		ids := strings.Split(line, ",")
+
+		for i := range ids {
+			id := strings.TrimSpace(ids[i])
+			parts := strings.Split(id, "-")
+			if len(parts) != 2 {
+				fmt.Printf("Invalid ID format: %s\n", id)
+				continue
+			}
+
+			startStr, endStr := strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1])
+			start, err1 := strconv.Atoi(startStr)
+			end, err2 := strconv.Atoi(endStr)
+
+			if err1 != nil || err2 != nil {
+				fmt.Printf("Invalid number in ID: %s\n", id)
+				continue
+			}
+
+			for num := start; num <= end; num++ {
+
+				numStr := strconv.Itoa(num)
+				if len(numStr)%2 == 0 {
+					mid := len(numStr) / 2
+					numParts := []string{numStr[:mid], numStr[mid:]}
+					fmt.Printf("Number %d split into: %v\n", num, numParts)
+
+					if len(numParts) != 2 {
+						fmt.Printf("Unexpected split result for number %d: %v\n", num, numParts)
+						continue
+					}
+
+					firstHalf, secondHalf := numParts[0], numParts[1]
+					if firstHalf == secondHalf {
+						fmt.Printf("Number %d has identical halves, \n", num)
+						totalSum += num
+					}
+				}
+
+			}
+
+		}
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Total sum: %d\n", totalSum)
+}
+
+func solvePart2() {
+	file, err := common.GetInputFile(2)
+
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	totalSum := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		ids := strings.Split(line, ",")
+
+		for i := range ids {
+			id := strings.TrimSpace(ids[i])
+			parts := strings.Split(id, "-")
+			if len(parts) != 2 {
+				fmt.Printf("Invalid ID format: %s\n", id)
+				continue
+			}
+
+			startStr, endStr := strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1])
+			start, err1 := strconv.Atoi(startStr)
+			end, err2 := strconv.Atoi(endStr)
+
+			if err1 != nil || err2 != nil {
+				fmt.Printf("Invalid number in ID: %s\n", id)
+				continue
+			}
+
+			for num := start; num <= end; num++ {
+				numStr := strconv.Itoa(num)
+				lenNum := len(numStr)
+				divisors := []int{}
+
+				for d := 1; d*d <= lenNum; d++ {
+					if lenNum%d == 0 {
+						if d > 1 {
+							divisors = append(divisors, d)
+						}
+						other := lenNum / d
+						if other > 1 && other != d {
+							divisors = append(divisors, other)
+						}
+					}
+				}
+
+				for _, div := range divisors {
+					partLen := lenNum / div
+					parts := []string{}
+
+					for i := 0; i < lenNum; i += partLen {
+						parts = append(parts, numStr[i:i+partLen])
+					}
+
+					allEqual := true
+					for j := 1; j < len(parts); j++ {
+						if parts[j] != parts[0] {
+							allEqual = false
+							break
+						}
+					}
+
+					if allEqual {
+						fmt.Printf("Number %d has all equal parts: %v\n", num, parts)
+						totalSum += num
+						break
+					}
+
+				}
+
+			}
+		}
+	}
+	fmt.Printf("Total sum: %d\n", totalSum)
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+}
