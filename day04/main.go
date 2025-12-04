e23b95f0fd890e3190e535c7b5054bb324ccda69
Miika Oinonen <mhtoin@utu.fi>
2025-12-04 22:03:33 +0200

    some optimisations

--- day04/main.go
+++ day04/main.go
@@ -6,6 +6,12 @@ import (
 	"github.com/mhtoin/advent-of-code-2025/common"
 )
 
+var directions = [8][2]int{
+	{-1, -1}, {-1, 0}, {-1, 1},
+	{0, -1}, {0, 1},
+	{1, -1}, {1, 0}, {1, 1},
+}
+
 func main() {
 	solvePart1()
 	solvePart2()
@@ -13,75 +19,45 @@ func main() {
 
 func solvePart1() {
 	lines := common.Read2DLines(4)
-	directions := [][2]int{
-		{-1, -1}, {-1, 0}, {-1, 1},
-		{0, -1}, {0, 1},
-		{1, -1}, {1, 0}, {1, 1},
-	}
-
-	totalAccessibleItems := 0
-
-	for i := range lines {
-		for j := range lines[i] {
-			item := lines[i][j]
-			if item == "@" {
-				amountOfInvalidNeighbors := 0
-
-				for _, dir := range directions {
-					if amountOfInvalidNeighbors >= 4 {
-						break
-					}
-					ni, nj := i+dir[0], j+dir[1]
-					if ni < 0 || ni >= len(lines) || nj < 0 || nj >= len(lines[i]) {
-						continue
-					}
-					neighbor := lines[ni][nj]
-					if neighbor == "@" {
-						amountOfInvalidNeighbors++
-					}
-				}
-				if amountOfInvalidNeighbors < 4 {
-					totalAccessibleItems++
-				}
-			}
-		}
-	}
-	fmt.Printf("Total accessible items: %d\n", totalAccessibleItems)
+	_, count := checkGrid(lines, false)
+	fmt.Printf("Total accessible items: %d\n", count)
 }
 
-func checkGrid(lines [][]string) ([][2]int, int) {
-	directions := [][2]int{
-		{-1, -1}, {-1, 0}, {-1, 1},
-		{0, -1}, {0, 1},
-		{1, -1}, {1, 0}, {1, 1},
+func checkGrid(lines [][]string, collectPositions bool) ([][2]int, int) {
+	rows := len(lines)
+	if rows == 0 {
+		return nil, 0
 	}
-	markedPositions := make([][2]int, 0)
+	cols := len(lines[0])
+
+	var markedPositions [][2]int
+	if collectPositions {
+		markedPositions = make([][2]int, 0, rows)
+	}
 
 	totalAccessibleItems := 0
 
-	for i := range lines {
-		for j := range lines[i] {
-			item := lines[i][j]
-			if item == "@" {
-				amountOfInvalidNeighbors := 0
+	for i := range rows {
+		for j := range cols {
+			if lines[i][j] != "@" {
+				continue
+			}
 
-				for _, dir := range directions {
-					if amountOfInvalidNeighbors >= 4 {
+			invalidNeighbors := 0
+			for _, dir := range directions {
+				ni, nj := i+dir[0], j+dir[1]
+				if ni >= 0 && ni < rows && nj >= 0 && nj < cols && lines[ni][nj] == "@" {
+					invalidNeighbors++
+					if invalidNeighbors >= 4 {
 						break
 					}
-					ni, nj := i+dir[0], j+dir[1]
-					if ni < 0 || ni >= len(lines) || nj < 0 || nj >= len(lines[i]) {
-						continue
-					}
-					neighbor := lines[ni][nj]
-					if neighbor == "@" {
-						amountOfInvalidNeighbors++
-					}
 				}
-				if amountOfInvalidNeighbors < 4 {
-					totalAccessibleItems++
-					markedPositions = append(markedPositions, [2]int{i, j})
+			}
 
+			if invalidNeighbors < 4 {
+				totalAccessibleItems++
+				if collectPositions {
+					markedPositions = append(markedPositions, [2]int{i, j})
 				}
 			}
 		}
@@ -89,29 +65,20 @@ func checkGrid(lines [][]string) ([][2]int, int) {
 	return markedPositions, totalAccessibleItems
 }
 
-func removeMarkedPositions(lines [][]string, markedPositions [][2]int) int {
-	removedCount := 0
-	for _, pos := range markedPositions {
-		i, j := pos[0], pos[1]
-		lines[i][j] = "x"
-		removedCount++
-	}
-	return removedCount
-}
-
 func solvePart2() {
 	lines := common.Read2DLines(4)
 	totalRemoved := 0
 
 	for {
-		markedPositions, totalAccessibleItems := checkGrid(lines)
-
-		if totalAccessibleItems == 0 {
+		markedPositions, count := checkGrid(lines, true)
+		if count == 0 {
 			break
 		}
 
-		removedCount := removeMarkedPositions(lines, markedPositions)
-		totalRemoved += removedCount
+		for _, pos := range markedPositions {
+			lines[pos[0]][pos[1]] = "x"
+		}
+		totalRemoved += count
 	}
 	fmt.Printf("Total removed items: %d\n", totalRemoved)
 }
